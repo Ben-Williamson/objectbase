2b97801f8be316bf019873520ed193b95bce4287
Ben Williamson <benmacwill@gmail.com>
2024-10-15 19:13:50 +0000

    Built support for reading and writing basic rows to the db.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,132 @@
-import {SupabaseClient} from "@supabase/supabase-js";
+import {PostgrestError, SupabaseClient} from "@supabase/supabase-js";
 
-export function wrapper(client: SupabaseClient): void {
-    console.log(client);
-}
+export class Table<RowObject extends Row>
+    {
+    supabase: SupabaseClient;
+    table_name: string;
+    rowConstructor: new (table: Table<RowObject>, content: any) => RowObject;
+
+    constructor(
+        supabase: SupabaseClient,
+        table_name: string,
+        rowConstructor: new (table: Table<RowObject>, content: any) => RowObject
+    )
+        {
+        this.supabase = supabase;
+        this.table_name = table_name;
+        this.rowConstructor = rowConstructor;
+        }
+
+    toString(): string
+        {
+        return `Objectbase table: ${this.table_name}`;
+        }
+
+    async select_all(): Promise<[RowObject[], PostgrestError | null]>
+        {
+        const {data, error} = await this.supabase
+            .from(this.table_name)
+            .select("*");
+
+        if (error) return [[], error];
+
+        return [data.map((item) => new this.rowConstructor(this, item)), null];
+        }
+    }
+
+export class Row
+    {
+    table: Table<any>;
+    content: any;
+    private is_locked: boolean;
+    private running_promises: Promise<boolean>[];
+
+    constructor(table: Table<any>, content: any)
+        {
+        this.table = table;
+        this.content = content;
+
+        this.is_locked = false;
+        this.running_promises = [];
+        }
+
+    private unlock(): void
+        {
+        console.log("unlock called");
+        this.is_locked = false;
+        this.running_promises = [];
+        }
+
+    private lock(promise: Promise<boolean>): void
+        {
+        if (!this.is_locked)
+            Promise.all(this.running_promises).finally(() => this.unlock());
+        this.is_locked = true;
+        this.running_promises.push(promise);
+        }
+
+    async sync(): Promise<boolean>
+        {
+        if (this.running_promises)
+            await Promise.all(this.running_promises);
+        return true;
+        }
+
+    ready_to_read(): boolean
+        {
+        return !this.is_locked;
+        }
+
+    async update_field(field: string | symbol, value: any)
+        {
+        async function updater(this_row: Row): Promise<boolean>
+            {
+            console.log("doing update for", field);
+            const { error } = await this_row.table.supabase
+                .from(this_row.table.table_name)
+                .update({[field]: value})
+                .eq('id', this_row.content.id)
+
+            if (error)
+                return false;
+
+            this_row.content[field] = value;
+            return true;
+            }
+
+        this.lock(updater(this));
+        }
+
+    }
+
+export class RowObjectProxy extends Row
+    {
+    constructor(table: Table<any>, content: any)
+        {
+        super(table, content);
+
+        return new Proxy(this, {
+            get: (target: this, prop: string | symbol) => {
+            // Check if the property exists on the instance
+            if (prop in target)
+            {
+                return (target as any)[prop];
+            }
+            if (target.ready_to_read() && prop in target.content)
+            {
+                return (target.content as any)[prop];
+            }
+            return undefined;
+            },
+            set: (target: this, prop: string | symbol, value: any) => {
+            if (prop === 'content')
+                {
+                this.content = value;
+                return true;
+                }
+            target.update_field(prop, value);
+            return true;
+            }
+        });
+        }
+    }
--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,63 @@
 import 'dotenv/config'
 
-import { createClient } from '@supabase/supabase-js'
-import {wrapper} from "../src";
+import {createClient, SupabaseClient} from '@supabase/supabase-js'
+import {Table, Row, RowObjectProxy} from "../src";
 
 const supabase = createClient(
     process.env.PUBLIC_SUPABASE_URL!,
     process.env.PUBLIC_SUPABASE_ANON_KEY!
 )
 
-// wrapper(supabase);
+class PetsTable extends Table<Pet>
+    {
+    constructor(supabase: SupabaseClient)
+        {
+        super(supabase, 'pets', Pet);
+        }
+    }
+
+class Pet extends RowObjectProxy
+    {
+    id!: number;
+    name!: string;
+    breed!: string;
+
+    constructor(table: Table<Pet>, content: any)
+        {
+        super(table, content);
+        }
+    }
+
+async function testit()
+    {
+    const { data, error: e } = await supabase.auth.signInWithPassword({
+        email: process.env.USER_EMAIL as string,
+        password: process.env.USER_PASSWORD as string,
+    });
+
+    let pets_table = new PetsTable(supabase);
+
+    let [pets, error] = await pets_table.select_all();
+
+    if (pets)
+        {
+        let pet = pets[0];
+
+        console.log(pet.id, pet.name, pet.breed);
+
+        // Looks like we might have occasional issues with
+        // not waiting for the last update.  This is likely
+        // due to how Promise.all().finally() is used under
+        // the hood.
+        pet.id = 14;
+        pet.name = "Pip";
+        pet.breed = "Russian Dwarf";
+
+        await pet.sync();
+
+        console.log(pet.id, pet.name, pet.breed);
+        }
+
+    }
+
+testit();
